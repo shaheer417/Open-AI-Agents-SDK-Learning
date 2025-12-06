91e38d5489de47d1a20fcaf4d2112552fdaf2407
shaheer417 <niazi2822@gmail.com>
2025-12-06 16:46:06 +0500

    feat: Integrate OpenAI agent and update dependencies

--- src/my_packaged_app/main.py
+++ src/my_packaged_app/main.py
@@ -1,7 +1,31 @@
-print("Top-level hello from main.py!")
+#  0. Importing the necessary libraries
+from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI
+
+import os
+from dotenv import load_dotenv, find_dotenv
 
 def main() -> None:
-    print("Hello from my-packaged-app! — running main.py")
+    # 0.1. Loading the environment variables
+    load_dotenv(find_dotenv())
+
+    # 1. Which LLM Provider to use? -> OpenAI Chat Completions API Service
+    external_client: AsyncOpenAI = AsyncOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+
+    # 2. Which LLM Model to use?
+    llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
+        model="gpt-3.5-turbo",
+        openai_client=external_client
+    )
+
+    # 3. Creating the Agent
+    agent: Agent = Agent(name="Assistant", model=llm_model)
+
+    # 4. Running the Agent
+    result = Runner.run_sync(starting_agent=agent, input="what is js")
+
+    print("AGENT RESPONSE: " , result.final_output)
 
 if __name__ == "__main__":
     main()
